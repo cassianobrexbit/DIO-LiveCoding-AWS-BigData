e0de8283502523bb933bdaa564488f8f34afb958
cassianobrexbit <cassiano@brexbit.com>
2021-06-10 11:59:35 -0300

    Commit 01

--- dio-live-wordcount-test.py
+++ dio-live-wordcount-test.py
@@ -9,7 +9,7 @@ class MRDataMining(MRJob):
     def steps(self):
         return [
             MRStep(mapper = self.mapper_get_words, reducer = self.reducer_count_words),
-            # MRStep(mapper = self.mapper_make_counts_key, reducer = self.reducer_output_words)
+            MRStep(mapper = self.mapper_make_counts_key, reducer = self.reducer_output_words)
         ]
 
     def mapper_get_words(self, _, line):
@@ -20,12 +20,12 @@ class MRDataMining(MRJob):
     def reducer_count_words(self, word, values):
         yield word, sum(values)
 
-    # def mapper_make_counts_key(self, word, count):
-    #     yield '%04d'%int(count), word
+    def mapper_make_counts_key(self, word, count):
+        yield '%04d'%int(count), word
 
-    # def reducer_output_words(self, count, words):
-    #     for word in words:
-    #         yield count, word
+    def reducer_output_words(self, count, words):
+        for word in words:
+            yield count, word
 
 if __name__ == '__main__':
     MRDataMining.run()
